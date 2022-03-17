2c286e24d825711f614e2297851e50972eb5e226
Cheyo Jimenez <cheyo@masters3d.com>
2022-03-17 13:42:34 -0700

    adding more overrides

--- keyboards/ergodox_ez/keymaps/masters3d/keymap.c
+++ keyboards/ergodox_ez/keymaps/masters3d/keymap.c
@@ -31,7 +31,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    ST_MACRO_0,     KC_1,           KC_GRAVE,       KC_MINUS,       KC_EQUAL,       KC_SLASH,       KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_BSLASH,      KC_LBRACKET,    KC_RBRACKET,    KC_LPRN,        KC_RPRN,        KC_TRANSPARENT,
+    ST_MACRO_0,     LSFT(KC_1),           KC_GRAVE,       KC_MINUS,       KC_EQUAL,       KC_SLASH,       KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_BSLASH,      KC_LBRACKET,    KC_RBRACKET,    KC_LPRN,        KC_RPRN,        KC_TRANSPARENT,
     KC_TRANSPARENT, KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
     KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_DOT,         KC_ENTER,
     KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_N,           KC_M,           KC_SCOLON,      KC_QUOTE,       KC_COMMA,       KC_TRANSPARENT,
@@ -109,16 +109,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 };
 
-
-// this is defined in another include.
-// #define KC_RIGHT_PAREN LSFT(KC_0) // )
-// #define KC_RPRN KC_RIGHT_PAREN
-
 const key_override_t backspace_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 const key_override_t dot_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, LSFT(KC_SCOLON));
 const key_override_t comma_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMMA, KC_SCOLON);
-const key_override_t parens_left_override = ko_make_basic(MOD_MASK_SHIFT, LSFT(KC_9), LSFT(KC_DOT));
-const key_override_t parens_right_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, LSFT(KC_COMMA));
+const key_override_t parens_left_override = ko_make_basic(MOD_MASK_SHIFT, LSFT(KC_9), LSFT(KC_COMMA));
+const key_override_t parens_right_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, LSFT(KC_DOT));
+const key_override_t number_1_override = ko_make_basic(MOD_MASK_SHIFT, LSFT(KC_1), LSFT(KC_SLASH));
+const key_override_t slash_override = ko_make_basic(MOD_MASK_SHIFT, KC_SLASH, LSFT(KC_5));
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -127,6 +124,8 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &comma_override,
     &parens_left_override,
     &parens_right_override,
+    &number_1_override,
+    &slash_override,
     NULL // Null terminate the array of overrides!
 };
 
